adcb6e4b44488233ba7764d37205a72eff9fe200
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-14 00:01:50 -0800

    added a simple flag parsing setup

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"os"
+)
+
+func main() {
+	var roleArn string
+
+	linkCmd := flag.NewFlagSet("link", flag.ExitOnError)
+	linkCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
+
+	credsCmd := flag.NewFlagSet("creds", flag.ExitOnError)
+	credsCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
+
+	if len(os.Args) < 2 {
+		informAndExit("This executable needs a subcommand and options to work. Use -h for help.", 1)
+	}
+}
