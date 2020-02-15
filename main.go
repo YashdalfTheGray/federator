f4e824b594eac8e676bfdd40183ec369e509ecf8
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-15 07:48:43 -0800

    added a variable for loading the creds into the environment

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func informAndExit(message string, code int) {
 
 func main() {
 	var roleArn string
+	var loadEnv bool
 
 	linkCmd := flag.NewFlagSet("link", flag.ExitOnError)
 	linkCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
 
 	credsCmd := flag.NewFlagSet("creds", flag.ExitOnError)
 	credsCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
+	credsCmd.BoolVar(&loadEnv, "load", false, "include this flag to load the creds into the environment")
 
 	if len(os.Args) < 2 {
 		informAndExit("This executable needs a subcommand and options to work. Use -h for help.", 1)
