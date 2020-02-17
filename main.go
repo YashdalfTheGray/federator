228d302773e2af21d139efd2a20c0219735a38ad
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-17 00:24:48 -0800

    we can't really load env for anything outside this program so removed

--- main.go
+++ main.go
@@ -55,14 +55,12 @@ func authWithSTS(roleArn string) (*sts.AssumeRoleOutput, error) {
 
 func main() {
 	var roleArn string
-	var loadEnv bool
 
 	linkCmd := flag.NewFlagSet("link", flag.ExitOnError)
 	linkCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
 
 	credsCmd := flag.NewFlagSet("creds", flag.ExitOnError)
 	credsCmd.StringVar(&roleArn, "role-arn", "", "the role arn to assume for federating with AWS")
-	credsCmd.BoolVar(&loadEnv, "load", false, "include this flag to load the creds into the environment")
 
 	if len(os.Args) < 2 {
 		informAndExit("This executable needs a subcommand and options to work. Use -h for help.", 1)
