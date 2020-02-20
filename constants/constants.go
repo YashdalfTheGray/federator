34ede4602fc3e5828cd8af1a959c6e8029544349
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-20 01:23:03 -0800

    added a module to contain the constants for the app

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+// These are some constants for the application
+const (
+	EnvAWSAccessKeyID     = "AWS_ACCESS_KEY_ID"
+	EnvAWSSecretAccessKey = "AWS_SECRET_ACCESS_KEY"
+	EnvAWSSessionToken    = "AWS_SESSION_TOKEN"
+	FederationEndpoint    = "https://signin.aws.amazon.com/federation"
+	DefaultIssuer         = "https://aws.amazon.com"
+	DefaultDestination    = "https://console.aws.amazon.com/console/home"
+)
