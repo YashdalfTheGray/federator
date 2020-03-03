864cce5e0423e9fde41870c49767d89d630e909d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-03-03 01:36:37 -0800

    added a constructor for creds details

--- models/creds_details.go
+++ models/creds_details.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/sts"
+)
 
 // CredsDetails holds all of the details we need to print
 // credentials once we have federated
@@ -9,4 +13,15 @@ type CredsDetails struct {
 	AccessKeyID     string     `json:"AccessKeyId"`
 	SecretAccessKey string     `json:"SecretAccessKey"`
 	SessionToken    string     `json:"SessionToken"`
+}
+
+// NewCredsDetails returns a new CredsDetails object with the
+// right things
+func NewCredsDetails(out *sts.AssumeRoleOutput) *CredsDetails {
+	return &CredsDetails{
+		ExpiresAfter:    out.Credentials.Expiration,
+		AccessKeyID:     *out.Credentials.AccessKeyId,
+		SecretAccessKey: *out.Credentials.SecretAccessKey,
+		SessionToken:    *out.Credentials.SessionToken,
+	}
 }
