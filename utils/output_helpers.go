4a2d680640a933b5166db39b02dc633557398024
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-03-05 08:04:12 -0800

    pulled in models and change up output based on flag

--- utils/output_helpers.go
+++ utils/output_helpers.go
@@ -6,13 +6,13 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/sts"
 
-	"github.com/YashdalfTheGray/federator/constants"
+	"github.com/YashdalfTheGray/federator/models"
 )
 
 // PrintCredsFromSTSOutput prints out the credentials we got from the
 // STS output in a way that the user can export them in to the shell
 // as well as the expiration information about the session
-func PrintCredsFromSTSOutput(out *sts.AssumeRoleOutput) {
+func PrintCredsFromSTSOutput(out *sts.AssumeRoleOutput, outputJSON bool) {
 	fmt.Println("Successfully authenticated with STS. Commands to use below.")
 
 	if os.Getenv("CI_MODE") == "true" {
@@ -20,15 +20,18 @@ func PrintCredsFromSTSOutput(out *sts.AssumeRoleOutput) {
 		return
 	}
 
-	fmt.Println(fmt.Sprintf("This session will expire at %s", out.Credentials.Expiration.Local().String()))
-	fmt.Println(fmt.Sprintf("export %s=%s", constants.EnvAWSAccessKeyID, *out.Credentials.AccessKeyId))
-	fmt.Println(fmt.Sprintf("export %s=%s", constants.EnvAWSSecretAccessKey, *out.Credentials.SecretAccessKey))
-	fmt.Println(fmt.Sprintf("export %s=%s", constants.EnvAWSSessionToken, *out.Credentials.SessionToken))
+	credsDetails := models.NewCredsDetails(out)
+
+	if outputJSON {
+		fmt.Println(credsDetails.ToJSONString())
+	} else {
+		fmt.Println(credsDetails.ToString())
+	}
 }
 
 // PrintLoginURLDetails prints out the login URL as well as the expiration date
 // of the session
-func PrintLoginURLDetails(out *sts.AssumeRoleOutput, loginURL string) {
+func PrintLoginURLDetails(out *sts.AssumeRoleOutput, loginURL string, outputJSON bool) {
 	fmt.Println("Successfully authenticated with STS. Login URL below.")
 
 	if os.Getenv("CI_MODE") == "true" {
@@ -36,6 +39,11 @@ func PrintLoginURLDetails(out *sts.AssumeRoleOutput, loginURL string) {
 		return
 	}
 
-	fmt.Println(fmt.Sprintf("This session will expire at %s", out.Credentials.Expiration.Local().String()))
-	fmt.Println(loginURL)
+	linkDetails := models.NewLinkDetails(out.Credentials.Expiration, loginURL)
+
+	if outputJSON {
+		fmt.Println(linkDetails.ToJSONString())
+	} else {
+		fmt.Println(linkDetails.ToString())
+	}
 }
