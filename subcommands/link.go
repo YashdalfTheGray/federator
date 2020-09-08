faf129bafe1bfe9399faf91e2a839622371c1c49
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-09-08 12:03:00 -0700

    added a new link subcommand type, organized parsed args better, exposed some functionality

--- subcommands/link.go
+++ subcommands/link.go
@@ -2,6 +2,7 @@ package subcommands
 
 import (
 	"flag"
+	"io"
 
 	"github.com/YashdalfTheGray/federator/constants"
 )
@@ -11,51 +12,63 @@ import (
 type LinkSubcommandParsedArgs struct {
 	RoleArn, ExternalID, Region, IssuerURL, DestinationURL string
 	OutputJSON                                             bool
-	subcommand                                             *flag.FlagSet
 }
 
-// NewLinkSubcommandParsedArgs creates an empty container for all the
-// data that will be set up by calling .Setup and wil be populated by
-// calling .Parse.
-func NewLinkSubcommandParsedArgs() LinkSubcommandParsedArgs {
+// LinkSubcommand holds the parsed args, when populated as well as internal
+// state that is needed to make this work.
+type LinkSubcommand struct {
+	Parsed     LinkSubcommandParsedArgs
+	subcommand *flag.FlagSet
+}
+
+func newLinkSubcommandParsedArgs() LinkSubcommandParsedArgs {
 	return LinkSubcommandParsedArgs{}
 }
 
+// NewLinkSubcommand creates an empty container for all the
+// data that will be set up by calling .Setup and wil be populated by
+// calling .Parse.
+func NewLinkSubcommand() LinkSubcommand {
+	return LinkSubcommand{
+		Parsed: newLinkSubcommandParsedArgs(),
+	}
+}
+
 // Setup will setup the subcommand with flags and descriptions.
-func (lpa LinkSubcommandParsedArgs) Setup() {
+func (cmd LinkSubcommand) Setup() {
 	linkCmd := flag.NewFlagSet("link", flag.ExitOnError)
 	linkCmd.StringVar(
-		&lpa.RoleArn,
+		&cmd.Parsed.RoleArn,
 		"role-arn",
 		"",
 		"the role arn to assume for federating with AWS",
 	)
 	linkCmd.StringVar(
-		&lpa.ExternalID,
+		&cmd.Parsed.ExternalID,
 		"external-id",
 		"",
 		"the external ID that can optionally be provided if the assume role requires it",
 	)
 	linkCmd.StringVar(
-		&lpa.Region,
+		&cmd.Parsed.Region,
 		"region",
 		"",
 		"the region to make the call against, will be read from the CLI config if omitted",
 	)
 	linkCmd.StringVar(
-		&lpa.IssuerURL,
+		&cmd.Parsed.IssuerURL,
 		"issuer",
 		constants.DefaultIssuer,
 		"the link where the user will be taken when the session has expired",
 	)
 	linkCmd.StringVar(
-		&lpa.DestinationURL,
+		&cmd.Parsed.DestinationURL,
 		"destination",
 		constants.DefaultDestination,
 		"the link that the user will be redirected to after login",
 	)
 	linkCmd.BoolVar(
-		&lpa.OutputJSON,
+		&cmd.Parsed.OutputJSON,
 		"json",
 		false,
 		"output results as JSON rather than plain text",
@@ -63,6 +76,16 @@ func (lpa LinkSubcommandParsedArgs) Setup() {
 }
 
 // Parse will parse the flags, according to the arguments setup in .Setup
-func (lpa LinkSubcommandParsedArgs) Parse(args []string) error {
-	return lpa.subcommand.Parse(args)
+func (cmd LinkSubcommand) Parse(args []string) error {
+	return cmd.subcommand.Parse(args)
+}
+
+// SetOutput is a mirror of flag.FlagSet.SetOutput
+func (cmd LinkSubcommand) SetOutput(output io.Writer) {
+	cmd.subcommand.SetOutput(output)
+}
+
+// PrintDefaults is a mirror of flag.FlagSet.PrintDefaults
+func (cmd LinkSubcommand) PrintDefaults() {
+	cmd.subcommand.PrintDefaults()
 }
