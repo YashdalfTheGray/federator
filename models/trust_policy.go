57c103a3e32081ac342d32345d4a07f1af823600
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-09-13 11:12:35 -0700

    sorted out the types a bit better

--- models/trust_policy.go
+++ models/trust_policy.go
@@ -1,16 +1,24 @@
 package models
 
+type statementCondition struct {
+	StringEquals struct {
+		ExternalID string `json:"sts:ExternalId"`
+	}
+}
+
+type policyStatement struct {
+	Effect    string
+	Resource  string
+	Action    []string
+	Principal struct {
+		AWS []string
+	}
+	Condition []statementCondition
+}
+
 // TrustPolicy models a trust policy that can be added
 // to an IAM role, although somewhat primitively
 type TrustPolicy struct {
 	Version   string
-	Statement []struct {
-		Effect    string
-		Resource  string
-		Action    []string
-		Principal struct {
-			AWS []string
-		}
-		Condition []struct{}
-	}
+	Statement []policyStatement
 }
