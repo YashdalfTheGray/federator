5055bb03dfa1212f49c02ebb223aa189392cac22
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-08-12 10:33:29 -0700

    updated the list of regions to validate against

--- helpers/validators.go
+++ helpers/validators.go
@@ -1,32 +1,53 @@
 package helpers
 
 // AvailableRegions is an array of regions that are supported
+// Sadly, since the endpoints package doesn't exist in v2, this
+// list is maintained by looking up the govcloud/china/iso
+// partitions manually, and then running an ec2 describe-regions
+// aws ec2 describe-regions --all-regions | jq '.Regions | .[] | .RegionName'
 var AvailableRegions = [...]string{
-	"af-south-1",
-	"ap-northeast-1",
-	"ap-southeast-2",
-	"aws-global",
-	"ca-central-1",
-	"eu-south-1",
-	"eu-west-3",
-	"me-south-1",
-	"ap-east-1",
+	// aws partition
+	"ap-south-2",
 	"ap-south-1",
-	"sa-east-1",
-	"us-east-2-fips",
-	"eu-north-1",
-	"eu-west-1",
-	"eu-west-2",
-	"us-east-1-fips",
-	"us-east-2",
-	"us-west-1-fips",
-	"ap-northeast-2",
-	"ap-southeast-1",
+	"eu-south-1",
+	"eu-south-2",
+	"me-central-1",
+	"il-central-1",
+	"ca-central-1",
+	"eu-central-1",
+	"eu-central-2",
 	"us-west-1",
 	"us-west-2",
-	"us-west-2-fips",
-	"eu-central-1",
+	"af-south-1",
+	"eu-north-1",
+	"eu-west-3",
+	"eu-west-2",
+	"eu-west-1",
+	"ap-northeast-3",
+	"ap-northeast-2",
+	"me-south-1",
+	"ap-northeast-1",
+	"sa-east-1",
+	"ap-east-1",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"ap-southeast-3",
+	"ap-southeast-4",
 	"us-east-1",
+	"us-east-2",
+
+	// govcloud partition
+	"us-gov-east-1",
+	"us-gov-west-1",
+
+	// china partition
+	"cn-north-1",
+	"cn-northwest-1",
+
+	// iso partitions
+	"us-iso-east-1",
+	"us-iso-west-1",
+	"us-isob-east-1",
 }
 
 // IsRegionValid validates the region passed in as a
